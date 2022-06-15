9fbe134902e59d3f4c4c2f980442de0d5609da87
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-15 12:43:07 +0200

    inizio header

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,6 +1,23 @@
 <template>
-    <header>
-        
+    <header class="header">
+        <div class="header__top">
+            <div class="header-logo">
+                <img src="../assets/img/logo_seo_w_1x.png" alt="">
+            </div>
+            <div class="header__nav">
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <button>ciao</button>
+            </div>
+        </div>
+        <div class="header__bottom">
+            
+        </div>
+
     </header>
 </template>
 
@@ -12,5 +29,43 @@ export default {
 
 <style lang='scss'>
 
+.header {
+
+    height: 300px;
+    background-image: url(../assets/img/1-hero-image.png);
+    background-repeat: no-repeat;
+    width: 100%;
+
+    &__top {
+        margin: auto;
+        max-width: var(--container-medium);
+        display: flex;
+        height: 50px;
+        justify-content: space-between;
+        align-items: center;
+        
+
+        .header-logo {
+            height: 100%;
+        }
+    }
+
+    &__bottom {
+        height: 300px;
+        display: flex;
+        background-image: url(../assets/img/1-hero-image.png);
+        background-repeat: no-repeat;
+
+        width: 100%;
+    }
+
+    &__nav {
+        display: flex;
+
+        a {
+            text-decoration: none;
+        }
+    }
+}
 
 </style>
