7570957f440c8e2bdcee0e7ffe07728c7e906e3f
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-16 11:47:27 +0200

    inizio section two background

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -1,18 +1,21 @@
 <template>
     <main class="main">
         <SectionOne/>
+        <SectionTwo/>
     </main>
 </template>
 
 <script>
 
 import SectionOne from './SectionOne.vue'
+import SectionTwo from './SectionTwo.vue'
 
 export default {
     name: 'BaseMain',
 
     components: {
         SectionOne,
+        SectionTwo,
     }
 }
 </script>
---
+++ src/components/SectionTwo.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="section">
+        <div class="section__top">
+
+        </div>
+        <div class="section__bottom">
+
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'SectionTwo'
+}
+</script>
+
+<style lang= 'scss' scoped>
+
+    .section {
+        
+        min-height: 200px;
+        display: flex;
+        flex-direction: column;
+        
+        &__top {
+            height: 650px;
+            background-image: url(../assets/img/pattern_background.png);
+            background-color: #051642;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+        }
+    }
+
+</style>
