989a63a0c7bf469567a86446270f3168924f89ba
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-16 17:25:37 +0200

    sezione 4 funzionante

--- src/components/SectionFour.vue
+++ src/components/SectionFour.vue
@@ -8,10 +8,60 @@
             <div class="card-section-3">
                 <h2>Standard</h2>
                 <div class="blue-number">
-
+                    <div class="dollaro">$</div>
+                    <div class="numero-grande">19</div>
+                    <div class="col-dx">
+                        <div class="novantanove">99</div>
+                        <div class="monthly">monthly</div>
+                    </div>
                 </div>
                 <p>15 Projects</p>
-                <p>30GB Storage</p>
+                <p class="gigabyte">30GB Storage</p>
+                <p>Unlimited Users</p>
+                <button class="btn">START TODAY</button>
+            </div>
+            <div class="card-section-3">
+                <h2>Standard</h2>
+                <div class="blue-number">
+                    <div class="dollaro">$</div>
+                    <div class="numero-grande">19</div>
+                    <div class="col-dx">
+                        <div class="novantanove">99</div>
+                        <div class="monthly">monthly</div>
+                    </div>
+                </div>
+                <p>15 Projects</p>
+                <p class="gigabyte">30GB Storage</p>
+                <p>Unlimited Users</p>
+                <button class="btn">START TODAY</button>
+            </div>
+            <div class="card-section-3">
+                <h2>Standard</h2>
+                <div class="blue-number">
+                    <div class="dollaro">$</div>
+                    <div class="numero-grande">19</div>
+                    <div class="col-dx">
+                        <div class="novantanove">99</div>
+                        <div class="monthly">monthly</div>
+                    </div>
+                </div>
+                <p>15 Projects</p>
+                <p class="gigabyte">30GB Storage</p>
+                <p>Unlimited Users</p>
+                <button class="btn">START TODAY</button>
+            </div>
+            <div class="card-section-3">
+                <h2>Standard</h2>
+                <div class="blue-number">
+                    <div class="dollaro">$</div>
+                    <div class="numero-grande">19</div>
+                    <div class="col-dx">
+                        <div class="novantanove">99</div>
+                        <div class="monthly">monthly</div>
+                    </div>
+                </div>
+                <p>15 Projects</p>
+                <p class="gigabyte">30GB Storage</p>
                 <p>Unlimited Users</p>
                 <button class="btn">START TODAY</button>
             </div>
@@ -64,22 +114,68 @@ export default {
                 background: white;
                 height: 300px;
                 width: 200px;
-                margin-top: 50px;
 
                 display: flex;
                 flex-direction: column;
+                align-items: center;
+                justify-content: center;
                 padding: 20px;
+                margin: 60px 10px;
+
+                .gigabyte {
+                    margin: 20px 0px;
+                }
 
                 .blue-number {
+                    display: flex;
+                    margin: 20px 0px;
+
+                    .dollaro {
+                        font-size: 11px;
+                        font-weight: 800;
+                        margin-top: 3px;
+                    }
+
+                    .numero-grande {
+                        font-size: 25px;
+                        font-weight: 800;
+                        color: var(--blue)
+                    }
+
+                    .col-dx {
+                        display: flex;
+                        flex-direction: column;
+                        align-items: flex-start;
+
+                        .novantanove {
+                            font-size: 9px;
+                            margin-top: 3px;
+                            font-weight: 800;
+                            color: var(--blue)
+                        }
+
+                        .monthly {
+                            font-size: 7px;
+                            color: var(--grey);
+                            margin-top: 2px;
+                            font-style: italic
+                        }
+                    }
 
                 }
 
                 .btn {
                     @include button-white;
+                    margin-top: 20px;
                 }
 
                 .btn:hover {
                     @include button-orange-bis;
+                }
+
+                p {
+                    color: var(--grey);
+                    font-size: 12px;
                 }
             }
         }
